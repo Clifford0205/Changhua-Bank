9c92866eac3d8c319e168645be1e787c0940a368
Clifford <ts02690140@gmail.com>
2020-01-20 11:52:57 +0800

    01201155

--- content/js/all.js
+++ content/js/all.js
@@ -4,4 +4,32 @@ $(document).ready(function() {
 
 
 
+let allnum=document.querySelectorAll('.num');
+let screen=document.querySelector('.screen');
+let clear =document.querySelector('.clear-btn');
+let deleteOne=document.querySelector('.delete-one');
+
+
+if(screen){
+    for(let i=0;i<allnum.length;i++){
+        allnum[i].addEventListener("click",function(){
+            console.log(this.value);
+            let num=this.value;
+            screen.value +=num;
+            console.log(screen.value.length);
+        },false)
+    }
+    
+    clear.addEventListener("click",function(){
+        screen.value="";
+    },false);
+    
+    deleteOne.addEventListener("click",function(){
+        var newStr= screen.value.slice(0,-1);
+        console.log(newStr);
+        screen.value=newStr;
+    },false 
+        
+    )
+}
 
